48a6f4dab7b382bd30d513d10de2850877596af4
Reynold <reynoldmorel@gmail.com>
2025-12-02 23:49:31 -0400

    Add http status accordgin to response status

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,10 +1,14 @@
 import { Express } from "express";
 import { validateAddress } from "./controllers/AddressController";
+import Status from "./types/Status";
 
 export const setupRoutes = (app: Express) => {
   app.post("/validate-address", async (req, res) => {
     const addressResponse = await validateAddress(req.body.address);
 
-    res.status(200).json(addressResponse).end();
+    res
+      .status(addressResponse.status === Status.UNVERIFIABLE ? 400 : 200)
+      .json(addressResponse)
+      .end();
   });
 };
